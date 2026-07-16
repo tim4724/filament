5f83bb68f2e8bb0cfb3f40301d1351b571aef3a5
Powei Feng <powei@google.com>
2026-07-16 16:49:52 -0700

    vk: set the renderpass name when DebugUtils is on
     - This PR also enables DebugUtils by default on debug builds.
       (when to enable it will be re-visited).
     - Separated validation from DebugUtils so that they are not
       tied together in enablement.
     - Together with Android Performance Analyzer, you should be able
       to see named renderpasses in the GPU non-fragment, fragment
       workload on the timeline.

--- filament/backend/src/vulkan/VulkanDriver.cpp
+++ filament/backend/src/vulkan/VulkanDriver.cpp
@@ -193,7 +193,7 @@ VKAPI_ATTR VkBool32 VKAPI_CALL debugReportCallback(VkDebugReportFlagsEXT flags,
 }
 #endif // FVK_ENABLED(FVK_DEBUG_VALIDATION)
 
-#if FVK_ENABLED(FVK_DEBUG_DEBUG_UTILS)
+#if FVK_ENABLED(FVK_DEBUG_DEBUG_UTILS) && FVK_ENABLED(FVK_DEBUG_VALIDATION)
 VKAPI_ATTR VkBool32 VKAPI_CALL debugUtilsCallback(VkDebugUtilsMessageSeverityFlagBitsEXT severity,
         VkDebugUtilsMessageTypeFlagsEXT types, const VkDebugUtilsMessengerCallbackDataEXT* cbdata,
         void* pUserData) {
@@ -2379,10 +2379,13 @@ void VulkanDriver::beginRenderPass(Handle<HwRenderTarget> rth, const RenderPassP
 // Assign a label to the framebuffer for debugging purposes.
 #if FVK_ENABLED(FVK_DEBUG_GROUP_MARKERS | FVK_DEBUG_DEBUG_UTILS)
     auto const topMarker = mCommands.getTopGroupMarker();
+    uint64_t fbVk = (uint64_t) vkfb->getVkFramebuffer();
+    uint64_t renderPassVk = (uint64_t) renderPass->getVkRenderPass();
     if (!topMarker.empty()) {
-        DebugUtils::setName(VK_OBJECT_TYPE_FRAMEBUFFER, reinterpret_cast<uint64_t>(vkfb),
-                topMarker.c_str());
+        DebugUtils::setName(VK_OBJECT_TYPE_FRAMEBUFFER, fbVk, topMarker.c_str());
+        DebugUtils::setName(VK_OBJECT_TYPE_RENDER_PASS, renderPassVk,topMarker.c_str());
     }
+
 #endif
 
     // The current command buffer now has references to the render target and its attachments.
